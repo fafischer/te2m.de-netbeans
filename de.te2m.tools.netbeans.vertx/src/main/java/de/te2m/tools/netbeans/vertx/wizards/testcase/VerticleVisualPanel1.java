99ccd8e8f9ee18ae90a6478cc2c72ea56a9af0da
Frank Fischer <frank@te2m.de>
2015-11-28 15:46:25 +0100

    wizard design adjusted

--- de.te2m.tools.netbeans.vertx/src/main/java/de/te2m/tools/netbeans/vertx/wizards/testcase/VerticleVisualPanel1.java
+++ de.te2m.tools.netbeans.vertx/src/main/java/de/te2m/tools/netbeans/vertx/wizards/testcase/VerticleVisualPanel1.java
@@ -21,39 +21,12 @@ import javax.swing.JPanel;
 public final class VerticleVisualPanel1 extends JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    /**
-     * The description text area.
-     */
     private javax.swing.JTextArea descriptionTextArea;
-
-    /**
-     * The j label1.
-     */
     private javax.swing.JLabel jLabel1;
-
-    /**
-     * The j label2.
-     */
     private javax.swing.JLabel jLabel2;
-
-    /**
-     * The j label3.
-     */
     private javax.swing.JLabel jLabel3;
-
-    /**
-     * The j scroll pane1.
-     */
     private javax.swing.JScrollPane jScrollPane1;
-
-    /**
-     * The name text field.
-     */
     private javax.swing.JTextField nameTextField;
-
-    /**
-     * The package text field.
-     */
     private javax.swing.JTextField packageTextField;
     // End of variables declaration//GEN-END:variables
 
@@ -141,20 +114,15 @@ public final class VerticleVisualPanel1 extends JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(jLabel1)
-                            .addGap(45, 45, 45)
-                            .addComponent(nameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(jLabel2)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(31, 31, 31)
-                        .addComponent(packageTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 288, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(22, Short.MAX_VALUE))
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 371, Short.MAX_VALUE)
+                    .addComponent(packageTextField)
+                    .addComponent(nameTextField))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -168,7 +136,7 @@ public final class VerticleVisualPanel1 extends JPanel {
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(packageTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
                 .addContainerGap(127, Short.MAX_VALUE))
