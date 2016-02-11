f45d3d83bde0e83bcf5095e60eff892c09987187
Frank Fischer <frank@te2m.de>
2016-02-11 20:53:45 +0100

    constants sorted

--- de.te2m.tools.netbeans.vertx/src/main/java/de/te2m/tools/netbeans/vertx/wizards/TemplateKeys.java
+++ de.te2m.tools.netbeans.vertx/src/main/java/de/te2m/tools/netbeans/vertx/wizards/TemplateKeys.java
@@ -18,17 +18,54 @@ package de.te2m.tools.netbeans.vertx.wizards;
 public interface TemplateKeys {
 
     /**
+     * The property creation date.
+     */
+    public static final String DN_CREATION_DATE = "CreationDate";
+    
+    /**
+     * The property description.
+     */
+    public static final String DN_DESCRIPTION = "TargetDescription";
+    
+    /**
+     * The Constant DN_DOCKER_IMAGE_NAME.
+     * Used for accessing the docker image name in the code generation properties
+     */
+    public static final String DN_DOCKER_IMAGE_NAME="DockerImageName";
+
+    /**
      * The Constant DN_MVN_ARTIFACT_ID.
      * Used for accessing the configured Maven artifact Id info in the code generation properties
      */
     public static final String DN_MVN_ARTIFACT_ID="artifactID";
-    
+
     /**
      * The Constant DN_MVN_GROUP_ID.
      * Used for accessing the configured Maven group ID info in the code generation properties
      */
     public static final String DN_MVN_GROUP_ID="groupID";
     
+    /**
+     * The Constant DN_POM_INFO.
+     * Used for accessing the POM info in the code generation properties
+     */
+    public static final String DN_POM_INFO="pomInfo";
+
+    /**
+     * The Constant DN_PROPERTY_CLASS_DESCRIPTION.
+     */
+    public static final String DN_PROPERTY_CLASS_DESCRIPTION = "TargetClassDescription";
+    
+    /**
+     * The Constant DN_PROPERTY_CLASS_NAME.
+     */
+    public static final String DN_PROPERTY_CLASS_NAME = "TargetClassName";
+    
+    /**
+     * The property package.
+     */
+    public static final String DN_PROPERTY_PACKAGE = "TargetPackage";
+
     /**
      * The Constant DN_VERTX_VERSION.
      */
@@ -45,7 +82,7 @@ public interface TemplateKeys {
      * Used for a flag for controlling the generation of a fat jar
      */
     public static final String GEN_CFG_CREATE_FAT_JAR="createFatJar";
-    
+
     /**
      * The Constant GEN_CFG_CREATE_VERTICLE_TEST.
      * Used for a flag for controlling the generation of a test case for a verticle
@@ -53,53 +90,26 @@ public interface TemplateKeys {
     public static final String GEN_CFG_CREATE_VERTICLE_TEST="cfgCreateVerticleTest";
 
     /**
-     * The Constant DN_DOCKER_IMAGE_NAME.
-     * Used for accessing the docker image name in the code generation properties
-     */
-    public static final String DN_DOCKER_IMAGE_NAME="DockerImageName";
-    
-    /**
-     * The Constant DN_POM_INFO.
-     * Used for accessing the POM info in the code generation properties
-     */
-    public static final String DN_POM_INFO="pomInfo";
-    
-    /**
      * The Constant PROPERTY_COMPANY.
      */
     public static final String PROPERTY_COMPANY="DefaultCompanyName";
-
+    
     /**
      * The Constant PROPERTY_COMPANY_URL.
      */
     public static final String PROPERTY_COMPANY_URL="DefaultCompanyURL";
-
-    /**
-     * The property creation date.
-     */
-    public static final String DN_CREATION_DATE = "CreationDate";
-
+    
+    
     /**
      * The Constant PROPERTY_DATE_FORMAT.
      */
     public static final String PROPERTY_DATE_FORMAT="DefaultDateFormat";
 
     /**
-     * The property description.
-     */
-    public static final String DN_DESCRIPTION = "TargetDescription";
-
-    /**
-     * The Constant DN_PROPERTY_CLASS_DESCRIPTION.
-     */
-    public static final String DN_PROPERTY_CLASS_DESCRIPTION = "TargetClassDescription";
-    
-    /**
      * The property folder.
      */
     public static final String PROPERTY_FOLDER = "TargetFolder";
-    
-    
+
     /**
      * The Constant PROPERTY_INCLUDE_COMPANY.
      */
@@ -109,26 +119,16 @@ public interface TemplateKeys {
      * The Constant PROPERTY_INCLUDE_USER.
      */
     public static final String PROPERTY_INCLUDE_USER="includeUserInfo";
-
+    
     /**
      * The property name.
      */
     public static final String PROPERTY_NAME = "TargetName";
-
-    /**
-     * The Constant DN_PROPERTY_CLASS_NAME.
-     */
-    public static final String DN_PROPERTY_CLASS_NAME = "TargetClassName";
     
     /**
      * The property name space.
      */
     public static final String PROPERTY_NAME_SPACE = "TargetNameSpace";
-    
-    /**
-     * The property package.
-     */
-    public static final String DN_PROPERTY_PACKAGE = "TargetPackage";
     
     /**
      * The property user.
